697ffd23f38681e55084f73c2d33d81730abc436
Shabbir1679 <shabbir.mohammed0@gmail.com>
2016-09-08 11:26:26 -0500

    Second Commit

---
+++ gituploads/example1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+a = 60
+
+b = 13
+
+c = 0
+
+c = a & b;
+
+print "Line 1 - Value of c is ", c
+
+c = a ^ b
+
+print "Line 2 - Value of c is", c
---
+++ practice/practice2/dictex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+a = 10
+
+b = 18
+
+c = 12
+
+c = a + b
+
+print "Line 1 - value of c is ", c
+
+
