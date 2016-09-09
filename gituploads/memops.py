4c358e0f960197a031eddaa6a68a5023be905b21
Shabbir1679 <shabbir.mohammed0@gmail.com>
2016-09-09 15:56:39 -0500

    New python file

---
+++ gituploads/memops.py
@@ -0,0 +1,21 @@
+#!/usr/bn/python
+
+
+a = 10
+
+b = 3
+
+list = [1, 2, 3, 4, 5, 9]
+
+if (a in list):
+    print "a is listed in the given list"
+
+else:
+    print "a is not listed in the given list"
+
+if (b not in list):
+    print "b is not listed in the given list"
+
+else:
+
+    print "b is listed in the given list"
